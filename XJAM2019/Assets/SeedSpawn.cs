06f6b6f21d1b456a4afcda61a58ba4a182a085b2
Grokher <grokherpub@gmail.com>
2019-12-18 09:36:36 +0100

    MoveMent script + TimerScript

---
+++ XJAM2019/Assets/SeedSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedSpawn : MonoBehaviour
+{
+    private int seeds;
+    private int plantedTrees;
+    private bool enoughSeeds;
+
+    public GameObject seed;
+
+    private void Start()
+    {
+        seeds = 10;
+        enoughSeeds = true;
+    }
+    private void Update()
+    {
+        if (enoughSeeds == true && seeds > 0)
+        {
+            Instantiate(seed, new Vector3(transform.position.x + Random.Range(-1f,1f), transform.position.y, transform.position.z), new Quaternion(0, 0, 0, 0));
+            seeds--;
+        }
+
+    }
+}
---
+++ XJAM2019/Assets/TimerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class TimerScript : MonoBehaviour
+{
+    public Text TimerText;
+    private float StartingTime;
+    private float timer;
+    void Start()
+    {
+        StartingTime = Time.time;
+    }
+
+    void Update()
+    {
+        //makes a timer in seconds and minutes
+
+        float time = Time.time - StartingTime;
+
+        string minutes = ((int)time / 60).ToString();
+        string seconds = (time % 60).ToString("f2");
+
+        TimerText.text = minutes + ":" + seconds;
+    }
+}
