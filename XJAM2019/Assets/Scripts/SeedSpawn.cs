ae276326b46ffd26f5fa7e81ceeabc7dba0b7296
Grokher <grokherpub@gmail.com>
2019-12-19 09:28:20 +0100

    added some stuff and fixed some stuff

---
+++ XJAM2019/Assets/Scripts/SeedSpawn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+
+public class SeedSpawn : MonoBehaviour
+{
+   [SerializeField] private int dollo;
+    private int seeds;
+    private int plantedTrees;
+    private bool enoughSeeds;
+    private int Planted = 0;
+    public Text SeedText;
+
+    public GameObject seed;
+
+    private void Start()
+    {
+        seeds = 10;
+        enoughSeeds = true;
+    }
+    private void Update()
+    {
+        Debug.Log(Planted);
+        if (enoughSeeds == true && seeds > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(seed, new Vector3(transform.position.x, transform.position.y, transform.position.z), new Quaternion(0, 0, 0, 0));
+            seeds--;
+            Planted++;
+            dollo += 10;
+        }
+        if (dollo >= 5)
+        {
+            seeds++;
+            dollo -= 5;
+        }
+        if (dollo >= 250)
+        {
+            //big bag
+        }
+        if (dollo >= 100)
+        {
+            //medium bagg
+        }
+        if (Planted >= 420)
+        {
+            End();
+        }
+        SeedText.text = "Seeds Planted: " + Planted.ToString();
+    }
+    private void End()
+    {
+        SceneManager.LoadScene("End");
+    }
+}
---
+++ XJAM2019/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class TimerScript : MonoBehaviour
+{
+    public Text TimerText;
+    private float StartingTime;
+    private float timer;
+    void Start()
+    {
+        StartingTime = Time.time;
+    }
+
+    void Update()
+    {
+        //makes a timer in seconds and minutes
+
+        float time = Time.time - StartingTime;
+
+        string minutes = ((int)time / 60).ToString();
+        string seconds = (time % 60).ToString("f2");
+
+        TimerText.text = minutes + ":" + seconds;
+        if(timer == 500)
+        {
+            SceneManager.LoadScene("End");
+        }
+    }
+}
